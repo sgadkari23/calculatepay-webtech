dc8b0a1b659bbd138ce8c3e79da9fd6f5404c7d8
Supriya G <sgadkar1@my.centennialcollege.ca>
2021-02-12 23:16:53 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  regularPay:number=0;
+  overTime:number=0;
+  totalPay:number=0;
+  taxOnPay:number=0;
+  hourlyrate:number=0;
+  noofhrsworked:number=0;
+  calculatePay(){
+     
+    this.noofhrsworked > 40 ? this.overTime = ((this.noofhrsworked - 40)*this.hourlyrate*1.5) : this.overTime = 0;
+
+    this.totalPay = this.overTime + this.noofhrsworked * this.hourlyrate;
+    this.regularPay = this.noofhrsworked * this.hourlyrate;
+    if(this.totalPay){
+      this.taxOnPay =this.totalPay * 0.18;
+    }   
+
+  }
 
   constructor() {}
 
